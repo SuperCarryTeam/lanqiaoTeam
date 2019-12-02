4ec3c797c51a0d9c4abd12a3b1d7eb4545102178
whl20170118 <610769312@qq.com>
2019-12-02 17:34:53 +0800

    Update APP.java

--- yykf/src/main/java/org/lanqiao/config/APP.java
+++ yykf/src/main/java/org/lanqiao/config/APP.java
@@ -4,4 +4,9 @@ public class APP {
     public static final String WXTOKEN = "";
     public static final String WXAPPID = "wx749011c267e2e1a5";
     public static final String WXAPPSECRET = "";
+    public static final String SMSURL = "dysmsapi.aliyuncs.com";
+    public static final String AccessKeyID = "LTAI4FdVTAduEFRYrfLRZmWa";
+    public static final String AccessKeySecret = "gPelDDv4hyCrwxFWSVIAxra4DtsJxW";
+    public static final String APPNAME = "预约看房";
+    public static final String SMSCODE = "SMS_179280148";
 }
