fdc083b07fd019c0ea329180266dc641e93827fe
Tool-man <58058217+Tool-man@users.noreply.github.com>
2019-12-02 19:17:13 +0800

    Update APP.java

--- yykf/src/main/java/org/lanqiao/config/APP.java
+++ yykf/src/main/java/org/lanqiao/config/APP.java
@@ -1,7 +1,7 @@
 package org.lanqiao.config;
 
 public class APP {
-    public static final String WXTOKEN = "";
+    public static final String WXTOKEN = "woaini";
     public static final String WXAPPID = "wx749011c267e2e1a5";
-    public static final String WXAPPSECRET = "";
+    public static final String WXAPPSECRET = "b085cea608e5be71b6246e86e17c460a";
 }
