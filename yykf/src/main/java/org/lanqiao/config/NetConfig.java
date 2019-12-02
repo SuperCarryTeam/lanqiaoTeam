bb7aa7f7951d8475c6e061184fb3d0938bacfea2
whl20170118 <610769312@qq.com>
2019-12-02 20:04:32 +0800

    Update NetConfig.java

--- yykf/src/main/java/org/lanqiao/config/NetConfig.java
+++ yykf/src/main/java/org/lanqiao/config/NetConfig.java
@@ -4,4 +4,6 @@ package org.lanqiao.config;
     所有网络请求地址
  */
 public class NetConfig {
+    public static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+    public static final String CREATE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
 }
