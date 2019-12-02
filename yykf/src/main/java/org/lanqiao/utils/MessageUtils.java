3e827205350889aef936d640ce562ea556b25261
whl20170118 <610769312@qq.com>
2019-12-02 10:25:08 +0800

    Update MessageUtils.java

--- yykf/src/main/java/org/lanqiao/utils/MessageUtils.java
+++ yykf/src/main/java/org/lanqiao/utils/MessageUtils.java
@@ -12,18 +12,6 @@ import java.util.*;
     微信消息解析类
  */
 public class MessageUtils {
-    public static final String MESSAGE_TEXT = "text";
-    public static final String MESSAGE_EVENT = "event";
-    public static final String MESSAGE_EVENT_SUBSCRIBE = "subscribe";
-    public static final String MESSAGE_IMAGE = "image";
-    public static final String MESSAGE_INEWS = "news";
-    public static final String MESSAGE_MUSIC = "music";
-    public static final String MESSAGE_EVENT_CLICK= "click";
-    public static final String MESSAGE_EVENT_VIEW= "view";
-    public static final String MESSAGE_EVENT_SCANCODE_PUSH= "scancode_push";
-    public static final String MESSAGE_EVENT_LOCATION_SELECT= "location_select";
-    public static final String MESSAGE_EVENT_LOCATION= "location";
-    public static final String MESSAGE_EVENT_SCAN= "SCAN";
 
     public static Map<String , String> xml2Map(HttpServletRequest request) throws IOException, DocumentException {
         Map<String , String> map = new HashMap<>();
